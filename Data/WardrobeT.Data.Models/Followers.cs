49d950b1e3b58971b5e0fdcb18c23fe0bc71d510
VencislavKA <57410823+VencislavKA@users.noreply.github.com>
2020-11-15 21:06:50 +0200

    Friends => Followers

---
+++ Data/WardrobeT.Data.Models/Followers.cs
@@ -0,0 +1,23 @@
+﻿namespace WardrobeT.Data.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    using Microsoft.AspNetCore.Identity;
+    using WardrobeT.Data.Common.Models;
+    using WardrobeT.Data.Models.Enums;
+
+    public class Followers : BaseModel<string>
+    {
+        public Followers()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        [Required]
+        public ApplicationUser User { get; set; }
+
+        [Required]
+        public ApplicationUser Followerd { get; set; }
+    }
+}
