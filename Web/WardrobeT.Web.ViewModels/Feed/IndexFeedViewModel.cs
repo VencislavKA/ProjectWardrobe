575c2245ecfebaf24b5cf75df71c6ae2497a0ae4
VencislavKA <57410823+VencislavKA@users.noreply.github.com>
2020-12-24 15:25:11 +0200

    new design at all

---
+++ Web/WardrobeT.Web.ViewModels/Feed/IndexFeedViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WardrobeT.Web.ViewModels.Feed
+{
+    public class IndexFeedViewModel
+    {
+        List<>
+    }
+}
